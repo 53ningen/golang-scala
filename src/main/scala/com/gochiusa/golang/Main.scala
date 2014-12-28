7084639f71d18bc21a9105e3186cc1207b87cc3b
53nigen <korun24276@gmail.com>
2014-12-28 15:56:56 +0900

    fix golang compile from command line

--- src/main/scala/com/gochiusa/golang/Main.scala
+++ src/main/scala/com/gochiusa/golang/Main.scala
@@ -2,22 +2,27 @@ package com.gochiusa.golang
 
 import com.gochiusa.golang.core.{Evaluator, AST, Environment, Parser}
 
+import scala.io.Source
+
 
 object Main {
   def main (args: Array[String]): Unit = {
     val parser = new Parser {
-      override protected val ifKeyword = "心ぴょんぴょん"
-      override protected val elseKeyword = "待ち？"
-      override protected val varKeyword = "心"
-      override protected val substituteKeyword = "ぴょんぴょん"
-      override protected val whileKeyword = "心ぴょんぴょん待ち？考えるフリしてもうちょっと！"
-      override protected val printKeyword = "心ぴょんぴょん可能"
+      override protected val ifKeyword = "心ぴょんぴょん待ち？"
+      override protected val elseKeyword = "ふわふわどきどき"
+      override protected val varKeyword = "らんらん"
+      override protected val substituteKeyword = "希望？"
+      override protected val whileKeyword = "マジ？"
+      override protected val printKeyword = "もふもふ"
+      override protected val trueKeyword = "ぴょんぴょん"
+      override protected val falseKeyword = "わわわ！"
     }
 
     val evaluator = new Evaluator
 
-    if (args.length > 1) {
-      val result = parser.parseAll(parser.stmts, args(1))
+    if (args.length > 0) {
+      val source = Source.fromFile(args(0))
+      val result = parser.parseAll(parser.stmts, source.mkString)
       evaluator.eval(result.get, Environment(Map()))
     }
 
